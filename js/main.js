a17af36676361aa02ca260f705fe972396c538e5
AlexChihaia <alexchihaia84@gmail.com>
2023-11-11 13:46:34 +0100

    feat : milestones

--- js/main.js
+++ js/main.js
@@ -19,4 +19,53 @@
 
 
 
-const images = [];
+const images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
+const items = document.querySelector('.items')
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+let currentItem = 0;
+
+for (let i = 0; i < images.length; i++) {
+        const item = document.createElement('div');
+        item.classList.add('item');
+
+        if (i === currentItem) {
+                item.classList.add('active');
+        }
+
+        const img = document.createElement('img');
+        img.src = `img/${images[i]}`;
+        img.alt = `Background${i}`;
+
+        item.append(img);
+        items.append(item);
+}
+
+
+const domItems = document.querySelectorAll('.item');
+
+
+prev.addEventListener('click', function () {
+        console.log('ho cliccato prev');
+        if (currentItem > 0) {
+                domItems[currentItem].classList.remove('active');
+
+                currentItem--;
+
+                domItems[currentItem].classList.add('active');
+        }
+
+})
+
+
+next.addEventListener('click', function () {
+        console.log('ho cliccato next');
+        if (currentItem < domItems.length - 1) {
+                domItems[currentItem].classList.remove('active');
+
+                currentItem++;
+
+                domItems[currentItem].classList.add('active');
+        }
+})
